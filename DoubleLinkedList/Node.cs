66684b4ef59de31dc1790d66d0928e9edbd11e4b
Srujana-Jami <srujana.jami@innroad.com>
2020-07-30 12:35:18 +0530

    Add project files.

---
+++ DoubleLinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoubleLinkedList
+{
+    class Node
+    {
+        public Node prev;
+        public int info;
+        public Node next;
+
+        public Node(int i)
+        {
+            info = i;
+            prev = null;
+            next = null;
+        }
+    }
+}
